7af3c8fd66bfd28810d5f2915b932d43327cbaea
NicolasSchoen <37415400+NicolasSchoen@users.noreply.github.com>
2018-10-12 19:45:30 +0200

    provinzendarstellung überarbeitet
    provinzen werden jetzt farblich angezeigt, um den besitzer darzustellen. Ausserdem werden im Angriffsmodus jetzt nur die Provinzen rot hervorgehoben, die anderen spielern gehören.

--- risiko_main.py
+++ risiko_main.py
@@ -16,7 +16,8 @@ print("zweimal wuerfeln =", wuerfel.wuerfeln(2))
 
 
 map = karte.Karte()
-
+map.felderInitialisieren()
+angriffvon = ""     #Hier wird Provinzid gespeichert, von der Angriff ausgeht
 
 
 
@@ -24,33 +25,56 @@ map = karte.Karte()
 
 def btn1func():
     w = map.drueckeRunde()
+    r = map.spielerAnReihe()
 
     if w == 0:
-        butt1.config(image = imgstart)
+        if r == 1:
+            butt1.config(image = imgstart)
+        elif r == 2:
+            butt1.config(image = imgstart2)
+        elif r == 3:
+            butt1.config(image = imgstart3)
+        elif r == 4:
+            butt1.config(image = imgstart4)
     elif w == 1:
         butt1.config(image=imgverst)
     elif w == 2:
         butt1.config(image=imgangriff)
     elif w == 3:
+        provinit()
         butt1.config(image=imgbewegen)
 
 
 def btnprovfunc(zahl):
     map.drueckeKnopf(zahl)
-    nachbarnZeigen(zahl)
+
+    print("Phase:", map.getPhase())
+    if (map.getPhase()[1] == 2):
+        nachbarnZeigen(zahl)
 
 
-def provblau():
+def provinit():
     for x in range(13):
         if x != 0:
-            butt[x].config(image = img2)
+            provinf = map.getProvInfo(x)
+            if(provinf[1] == 1):
+                butt[x].config(bg="blue")
+            elif(provinf[1] == 2):
+                butt[x].config(bg="yellow")
+            elif (provinf[1] == 3):
+                butt[x].config(bg="orange")
+            elif (provinf[1] == 4):
+                butt[x].config(bg="green")
+
 
 
 def nachbarnZeigen(provid):
-    provblau()
+    provinit()
+    angriffvon = provid
     nbrn = map.nachbarn(provid)
     for x in range(len(nbrn)):
-        butt[nbrn[x]].config(image = provrot)
+        if(map.getProvInfo(provid)[1] != map.getProvInfo(nbrn[x])[1]):
+            butt[nbrn[x]].config(bg="red")
 
 
 
@@ -65,12 +89,13 @@ theLabel.pack()
 
 
 imgstart = PhotoImage(file="..\\karte\\start.png")
+imgstart2 = PhotoImage(file="..\\karte\\start2.png")
+imgstart3 = PhotoImage(file="..\\karte\\start3.png")
+imgstart4 = PhotoImage(file="..\\karte\\start4.png")
 imgverst = PhotoImage(file="..\\karte\\verstaerkung.png")
 imgangriff = PhotoImage(file="..\\karte\\angriff.png")
 imgbewegen = PhotoImage(file="..\\karte\\bewegen.png")
 gif1 = PhotoImage(file="..\\karte\\schiessen.gif")
-img2 = PhotoImage(file="..\\karte\\provinz.png")
-provrot = PhotoImage(file="..\\karte\\provinzrot.png")
 butt1 = Button(root, image=imgstart, text="start", command= lambda : btn1func())
 
 butt1.pack()
@@ -88,41 +113,42 @@ C.create_image(0,0, anchor=NW, image=img)
 #########################################################################################
 butt = [Button(), Button(), Button(), Button(), Button(), Button(), Button(), Button(), Button(), Button(), Button(), Button(), Button()]
 #provinz1
-butt[1] = Button(root, image=img2, text="start", command= lambda : btnprovfunc(1))
+butt[1] = Button(root, bg="blue", text="1", command= lambda : btnprovfunc(1))
 butt[1].place(x=135, y=180)
 #provinz2
-butt[2] = Button(root, image=img2, text="start", command= lambda : btnprovfunc(2))
+butt[2] = Button(root, bg="blue", text="1", command= lambda : btnprovfunc(2))
 butt[2].place(x=70, y=220)
 #provinz3
-butt[3] = Button(root, image=img2, text="start", command= lambda : btnprovfunc(3))
+butt[3] = Button(root, bg="blue", text="1", command= lambda : btnprovfunc(3))
 butt[3].place(x=135, y=280)
 #provinz4
-butt[4] = Button(root, image=img2, text="start", command= lambda : btnprovfunc(4))
+butt[4] = Button(root, bg="blue", text="1", command= lambda : btnprovfunc(4))
 butt[4].place(x=120, y=350)
 #provinz5
-butt[5] = Button(root, image=img2, text="start", command= lambda : btnprovfunc(5))
+butt[5] = Button(root, bg="blue", text="1", command= lambda : btnprovfunc(5))
 butt[5].place(x=250, y=480)
 #provinz6
-butt[6] = Button(root, image=img2, text="start", command= lambda : btnprovfunc(6))
+butt[6] = Button(root, bg="blue", text="1", command= lambda : btnprovfunc(6))
 butt[6].place(x=450, y=250)
 #provinz7
-butt[7] = Button(root, image=img2, text="start", command= lambda : btnprovfunc(7))
+butt[7] = Button(root, bg="blue", text="1", command= lambda : btnprovfunc(7))
 butt[7].place(x=650, y=400)
 #provinz8
-butt[8] = Button(root, image=img2, text="start", command= lambda : btnprovfunc(8))
+butt[8] = Button(root, bg="blue", text="1", command= lambda : btnprovfunc(8))
 butt[8].place(x=500, y=405)
 #provinz9
-butt[9] = Button(root, image=img2, text="start", command= lambda : btnprovfunc(9))
+butt[9] = Button(root, bg="blue", text="1", command= lambda : btnprovfunc(9))
 butt[9].place(x=550, y=610)
 #provinz10
-butt[10] = Button(root, image=img2, text="start", command= lambda : btnprovfunc(10))
+butt[10] = Button(root, bg="blue", text="1", command= lambda : btnprovfunc(10))
 butt[10].place(x=400, y=650)
 #provinz11
-butt[11] = Button(root, image=img2, text="start", command= lambda : btnprovfunc(11))
+butt[11] = Button(root, bg="blue", text="1", command= lambda : btnprovfunc(11))
 butt[11].place(x=200, y=650)
 #provinz12
-butt[12] = Button(root, image=img2, text="start", command= lambda : btnprovfunc(12))
+butt[12] = Button(root, bg="blue", text="1", command= lambda : btnprovfunc(12))
 butt[12].place(x=220, y=150)
 
-root.mainloop()
+provinit()
 
+root.mainloop()
