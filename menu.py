d91207189021c0f74d3857343b689b86e4aeb769
NicolasSchoen <37415400+NicolasSchoen@users.noreply.github.com>
2018-11-06 10:34:07 +0100

    singleplayer in menü implementiert
    jetzt läuft spiel von der auswahl der spielmodi bis zum singleplayer

--- menu.py
+++ menu.py
@@ -20,6 +20,9 @@ class SampleApp(tk.Tk):
         container.pack(side="top", fill="both", expand=True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
+        self.title("Risiko")
+
+        map = karte.Karte()
 
         self.frames = {}
         for F in (StartPage, Singleplayer, Multiplayer, Host, GuiMap, Join):
@@ -45,7 +48,7 @@ class StartPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
-        label = tk.Label(self, text="Risiko v0.1", font=controller.title_font)
+        label = tk.Label(self, text="Risiko alpha 0.2", font=controller.title_font)
         label.pack(side="top", fill="x", pady=10)
         label2 = tk.Label(self, text="Nicolas Schön | Johannes Wimmer")
         label2.pack(side="top", fill="x", pady=10)
@@ -69,8 +72,15 @@ class Singleplayer(tk.Frame):
                            command=lambda: controller.show_frame("StartPage"))
         button.pack()
 
-        button2 = tk.Button(self, text="Starte (1 Gegner)", command=lambda: controller.show_frame("GuiMap"))
+        w = tk.Scale(self, from_=2, to=4, orient="horizontal")
+        w.pack()
+
+        button2 = tk.Button(self, text="Starte", command=lambda: controller.show_frame("GuiMap"))
         button2.pack()
+
+    #def buttonStarte(self):
+    #    map = karte.Karte(self.w.get())
+    #    self.controller.show_frame("GuiMap")
 
 
 class Multiplayer(tk.Frame):
@@ -160,8 +170,6 @@ class GuiMap(tk.Frame):
         button = tk.Button(self, text="Main-Menu",
                            command=lambda: controller.show_frame("StartPage"))
         button.pack()
-        label = tk.Label(self, text="Risiko v0.2")
-        label.pack(side="top", fill="x", pady=10)
 
 
 
@@ -170,73 +178,73 @@ class GuiMap(tk.Frame):
         #theLabel = tk.Label(self, text="Risiko")
         #theLabel.pack()
 
-        imganfang = tk.PhotoImage(file="..\\karte\\anfang.png")
-        imgstart = tk.PhotoImage(file="..\\karte\\start.png")
-        imgstart2 = tk.PhotoImage(file="..\\karte\\start2.png")
-        imgstart3 = tk.PhotoImage(file="..\\karte\\start3.png")
-        imgstart4 = tk.PhotoImage(file="..\\karte\\start4.png")
-        imgverst = tk.PhotoImage(file="..\\karte\\verstaerkung.png")
-        imgangriff = tk.PhotoImage(file="..\\karte\\angriff.png")
-        imgbewegen = tk.PhotoImage(file="..\\karte\\bewegen.png")
-        gif1 = tk.PhotoImage(file="..\\karte\\schiessen.gif")
-        butt1 = tk.Button(self, image=imganfang, text="start", command=lambda: self.btn1func())
+        self.imganfang = tk.PhotoImage(file="..\\karte\\anfang.png")
+        self.imgstart = tk.PhotoImage(file="..\\karte\\start.png")
+        self.imgstart2 = tk.PhotoImage(file="..\\karte\\start2.png")
+        self.imgstart3 = tk.PhotoImage(file="..\\karte\\start3.png")
+        self.imgstart4 = tk.PhotoImage(file="..\\karte\\start4.png")
+        self.imgverst = tk.PhotoImage(file="..\\karte\\verstaerkung.png")
+        self.imgangriff = tk.PhotoImage(file="..\\karte\\angriff.png")
+        self.imgbewegen = tk.PhotoImage(file="..\\karte\\bewegen.png")
+        self.gif1 = tk.PhotoImage(file="..\\karte\\schiessen.gif")
+        self.butt1 = tk.Button(self, image=self.imganfang, text="start", command=lambda: self.btn1func())
 
-        butt1.pack()
+        self.butt1.pack()
 
         C = tk.Canvas(self, height=self.hoehe, width=self.breite)
 
 
-        img = tk.PhotoImage(file="..\\karte\\map_small.png")
-        C.create_image(0, 0, anchor= tk.NW, image=img)
+        self.img = tk.PhotoImage(file="..\\karte\\map_small.png")
+        C.create_image(0, 0, anchor= tk.NW, image=self.img)
         C.pack()
 
         # absolute platzierung der provinz-buttons
         #########################################################################################
-        butt = [tk.Button(), tk.Button(), tk.Button(), tk.Button(), tk.Button(), tk.Button(), tk.Button(), tk.Button(), tk.Button(), tk.Button(),
+        self.butt = [tk.Button(), tk.Button(), tk.Button(), tk.Button(), tk.Button(), tk.Button(), tk.Button(), tk.Button(), tk.Button(), tk.Button(),
                 tk.Button(), tk.Button(), tk.Button()]
         # provinz1
-        butt[1] = tk.Button(self, bg="blue", text="1", command=lambda: self.btnprovfunc(1))
-        butt[1].place(x=135, y=180)
+        self.butt[1] = tk.Button(self, bg="blue", text="1", command=lambda: self.btnprovfunc(1))
+        self.butt[1].place(x=135, y=180)
         # provinz2
-        butt[2] = tk.Button(self, bg="blue", text="1", command=lambda: self.btnprovfunc(2))
-        butt[2].place(x=70, y=220)
+        self.butt[2] = tk.Button(self, bg="blue", text="1", command=lambda: self.btnprovfunc(2))
+        self.butt[2].place(x=70, y=220)
         # provinz3
-        butt[3] = tk.Button(self, bg="blue", text="1", command=lambda: self.btnprovfunc(3))
-        butt[3].place(x=135, y=280)
+        self.butt[3] = tk.Button(self, bg="blue", text="1", command=lambda: self.btnprovfunc(3))
+        self.butt[3].place(x=135, y=280)
         # provinz4
-        butt[4] = tk.Button(self, bg="blue", text="1", command=lambda: self.btnprovfunc(4))
-        butt[4].place(x=120, y=350)
+        self.butt[4] = tk.Button(self, bg="blue", text="1", command=lambda: self.btnprovfunc(4))
+        self.butt[4].place(x=120, y=350)
         # provinz5
-        butt[5] = tk.Button(self, bg="blue", text="1", command=lambda: self.btnprovfunc(5))
-        butt[5].place(x=250, y=480)
+        self.butt[5] = tk.Button(self, bg="blue", text="1", command=lambda: self.btnprovfunc(5))
+        self.butt[5].place(x=250, y=480)
         # provinz6
-        butt[6] = tk.Button(self, bg="blue", text="1", command=lambda: self.btnprovfunc(6))
-        butt[6].place(x=450, y=250)
+        self.butt[6] = tk.Button(self, bg="blue", text="1", command=lambda: self.btnprovfunc(6))
+        self.butt[6].place(x=450, y=250)
         # provinz7
-        butt[7] = tk.Button(self, bg="blue", text="1", command=lambda: self.btnprovfunc(7))
-        butt[7].place(x=650, y=400)
+        self.butt[7] = tk.Button(self, bg="blue", text="1", command=lambda: self.btnprovfunc(7))
+        self.butt[7].place(x=650, y=400)
         # provinz8
-        butt[8] = tk.Button(self, bg="blue", text="1", command=lambda: self.btnprovfunc(8))
-        butt[8].place(x=500, y=405)
+        self.butt[8] = tk.Button(self, bg="blue", text="1", command=lambda: self.btnprovfunc(8))
+        self.butt[8].place(x=500, y=405)
         # provinz9
-        butt[9] = tk.Button(self, bg="blue", text="1", command=lambda: self.btnprovfunc(9))
-        butt[9].place(x=550, y=610)
+        self.butt[9] = tk.Button(self, bg="blue", text="1", command=lambda: self.btnprovfunc(9))
+        self.butt[9].place(x=550, y=610)
         # provinz10
-        butt[10] = tk.Button(self, bg="blue", text="1", command=lambda: self.btnprovfunc(10))
-        butt[10].place(x=400, y=650)
+        self.butt[10] = tk.Button(self, bg="blue", text="1", command=lambda: self.btnprovfunc(10))
+        self.butt[10].place(x=400, y=650)
         # provinz11
-        butt[11] = tk.Button(self, bg="blue", text="1", command=lambda: self.btnprovfunc(11))
-        butt[11].place(x=200, y=650)
+        self.butt[11] = tk.Button(self, bg="blue", text="1", command=lambda: self.btnprovfunc(11))
+        self.butt[11].place(x=200, y=650)
         # provinz12
-        butt[12] = tk.Button(self, bg="blue", text="1", command=lambda: self.btnprovfunc(12))
-        butt[12].place(x=220, y=150)
+        self.butt[12] = tk.Button(self, bg="blue", text="1", command=lambda: self.btnprovfunc(12))
+        self.butt[12].place(x=220, y=150)
 
         #self.provinit()
         #self.mainloop()
 
     def btn1func(self):
-        w = map.drueckeRunde()
-        r = map.spielerAnReihe()
+        w = self.map.drueckeRunde()
+        r = self.map.spielerAnReihe()
         self.provinit()
 
         # setze Farbe des Rundenbuttons
@@ -269,20 +277,20 @@ class GuiMap(tk.Frame):
     def btnprovfunc(self, zahl):
         self.provinit()
 
-        rueckgabe = map.drueckeKnopf(zahl, self.aktiverSpieler)
+        rueckgabe = self.map.drueckeKnopf(zahl, self.aktiverSpieler)
         if (rueckgabe == None):
 
-            print("Phase:", map.getPhase())
+            print("Phase:", self.map.getPhase())
 
-            if (map.getBesitzer(zahl) == self.aktiverSpieler):
-                if (map.getPhase()[1] == 2):
+            if (self.map.getBesitzer(zahl) == self.aktiverSpieler):
+                if (self.map.getPhase()[1] == 2):
                     self.nachbarnZeigen(1, zahl)
-                elif (map.getPhase()[1] == 3):
+                elif (self.map.getPhase()[1] == 3):
                     self.nachbarnZeigen(2, zahl)
-                elif (map.getPhase()[1] == 1):
+                elif (self.map.getPhase()[1] == 1):
                     self.provinit()
         elif (rueckgabe[0] == "angriff"):
-            msg = "Schlacht von " + map.nameVon(zahl)
+            msg = "Schlacht von " + self.map.nameVon(zahl)
             print(msg)
             showinfo("Angriff", msg)
             self.provinit()
@@ -292,8 +300,8 @@ class GuiMap(tk.Frame):
     def provinit(self):
         for x in range(13):
             if x != 0:
-                self.butt[x].config(text=map.getProvInfo(x)[0])
-                provinf = map.getProvInfo(x)
+                self.butt[x].config(text=self.map.getProvInfo(x)[0])
+                provinf = self.map.getProvInfo(x)
                 if (provinf[1] == 1):
                     self.butt[x].config(bg="lightblue")
                 elif (provinf[1] == 2):
@@ -305,13 +313,13 @@ class GuiMap(tk.Frame):
 
     def nachbarnZeigen(self, modus, provid):  # modus = 1|2: angriff oder bewegen; privid = Provinz
         angriffvon = provid
-        nbrn = map.nachbarn(provid)
+        nbrn = self.map.nachbarn(provid)
         for x in range(len(nbrn)):
             if (modus == 1):
-                if (map.getProvInfo(provid)[1] != map.getProvInfo(nbrn[x])[1]):
+                if (self.map.getProvInfo(provid)[1] != self.map.getProvInfo(nbrn[x])[1]):
                     self.butt[nbrn[x]].config(bg="red")
             elif (modus == 2):
-                if (map.getProvInfo(provid)[1] == map.getProvInfo(nbrn[x])[1]):
+                if (self.map.getProvInfo(provid)[1] == self.map.getProvInfo(nbrn[x])[1]):
                     self.butt[nbrn[x]].config(bg="black")
 
 
